51f090e7462e64f7a8d5ac992e0353f944919b98
Amila Sikalo <asikalo2@etf.unsa.ba>
2018-10-23 16:10:26 +0200

    Zadatak 2

---
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -0,0 +1,29 @@
+package ba.unsa.etf.rpr.tutorijal02;
+
+public class Interval {
+
+    void toString() {
+
+    }
+
+    void isIn() {
+
+    }
+
+
+    void isNull() {
+
+    }
+
+
+    void intersect() {
+
+    }
+
+
+    void equals() {
+
+    }
+
+
+}
